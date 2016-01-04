c7e7c8e68c27be267f848797007f478d54dc5e95
dmichel <david.michel@tocea.com>
2016-01-04 11:10:35 +0100

    update author

--- init.rb
+++ init.rb
@@ -1,12 +1,16 @@
 Redmine::Plugin.register :redmine_scrum_cards do
+  
   name 'Redmine Scrum Cards plugin'
-  author 'Echoes Labs'
+  author 'Echoes'
   description 'Plugin for generating printable scrum cards'
   version '0.1'
   url 'https://github.com/echoes-tech/redmine_scrum_cards'
   author_url 'https://github.com/echoes-tech'
   
-  settings :default => { 'tester_field_name' => '', 'date_formatter' => "%d/%m/%Y" }, :partial => 'settings/scrum_cards_settings'
+  settings :default => { 
+    'tester_field_name' => '', 
+    'date_formatter' => "%d/%m/%Y" 
+  }, :partial => 'settings/scrum_cards_settings'
   
 end
 
